4fbfbcbbb16d8fbed8bc43c59a5b73a3e1e90fda
Hai Alaluf <hai@kidbox.com>
2017-08-30 16:33:58 +0300

    dd files for google and facebook login

--- config/auth.js
+++ config/auth.js
@@ -4,11 +4,11 @@
 module.exports = {
     facebook: {
         clientID: '566983050092492',
-        clientSecret: '***************',
+        clientSecret: 'b078835de15617594f41927313f29964',
         callbackUrl: 'http://127.0.0.1:1818/auth/facebook/callback'
     },
     google: {
         clientID: '782172464302-up9run1lv6e2vrhlq0acrupq2kf68oru.apps.googleusercontent.com',
-        clientSecret: '***************',
+        clientSecret: 'M9tWWD5RtrzQYp5uJaAljzdR',
     }
 }
